e23271f46719eb8dc845e96b34fc55f9ca8a509e
nomad86 <landru86@gmail.com>
2016-10-20 12:54:14 -0400

    Added urgent page

--- Module_Four/Presentation/Resource4Me/App/app.module.js
+++ Module_Four/Presentation/Resource4Me/App/app.module.js
@@ -1,8 +1,8 @@
 ﻿angular.module('app', [
     'app.hp',
     'app.fr',
-    'app.ui'
-    ,'app.layout'
-    , 'app.core'
-   
+    'app.ui',
+    'app.layout',
+    'app.core',
+    'app.urgent'
 ]);
